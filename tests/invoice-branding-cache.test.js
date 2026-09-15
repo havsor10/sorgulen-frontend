e6d8b229bb203b3934c93eb3dcdde23c18469ddd
havsor10 <havard1009@gmail.com>
2026-09-15 10:44:38 +0200

    Oppdater faktura cachetest

--- tests/invoice-branding-cache.test.js
+++ tests/invoice-branding-cache.test.js
@@ -5,6 +5,7 @@ const path = require("node:path");
 
 const html = fs.readFileSync(path.resolve(__dirname, "../admin/faktura-detalj.html"), "utf8");
 
-test("invoice detail controller is cache-busted after validation change", () => {
-  assert.match(html, /faktura-detalj\.js\?v=20260915-override1/);
+test("invoice detail controller is cache-busted after workflow cleanup", () => {
+  assert.match(html, /faktura-detalj\.js\?v=20260915-audit1/);
+  assert.match(html, /invoice-flow-20260915-audit1/);
 });
